1d002f866d4924ec729d9e1fd499d49fa828104c
Alex Brewster <alexgbrewster@gmail.com>
2024-03-04 17:57:39 -0500

    creating authentication

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -153,7 +153,7 @@ export default function Login() {
             href="#"
             className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
           >
-            Start a 14 day free trial
+            Start your 3 month free trial
           </a>
         </p>
       </div>
