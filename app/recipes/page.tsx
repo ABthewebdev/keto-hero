5d1c10ed7bbb73b5cc1a97abba6048b256ea517c
Alex Brewster <alexgbrewster@gmail.com>
2024-03-30 21:43:08 -0400

    creating recipes page

--- app/recipes/page.tsx
+++ app/recipes/page.tsx
@@ -2,8 +2,9 @@ import Image from "next/image";
 import Link from "next/link";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { SearchIcon } from "lucide-react";
+import { SearchIcon, Star } from "lucide-react";
 import { recipesData } from "../lib/placeholder-data";
+import { StarIcon } from "lucide-react";
 
 const page = () => {
   return (
@@ -12,7 +13,6 @@ const page = () => {
         <h1 className="text-xl font-medium">Delicious Recipes</h1>
         <form className="flex items-center gap-4 md:gap-8">
           <div className="flex items-center gap-2 w-full md:w-[400px]">
-            <SearchIcon className="h-4 w-4 text-gray-500 dark:text-gray-400" />
             <Input
               className="font-normal bg-white text-base w-full"
               placeholder="Search for food..."
@@ -24,10 +24,10 @@ const page = () => {
           </Button>
         </form>
       </div>
-      <section className="flex pt-8 justify-center gap-x-10 flex-wrap">
+      <section className="flex pt-8 justify-center gap-x-12 flex-wrap">
         {recipesData.map((recipe) => (
           <Link href="/" key={recipe.id}>
-            <article className="text-center text-base">
+            <article className="text-base font-medium">
               <Image
                 className="w-56 h-56"
                 src={recipe.imageSrc}
@@ -36,6 +36,13 @@ const page = () => {
                 height={220}
               />
               <h2>{recipe.name}</h2>
+              <div className="flex">
+                <StarIcon className="w-4 h-4 fill-primary" />
+                <StarIcon className="w-4 h-4 fill-primary" />
+                <StarIcon className="w-4 h-4 fill-primary" />
+                <StarIcon className="w-4 h-4 fill-primary" />
+                <StarIcon className="w-4 h-4" />
+              </div>
             </article>
           </Link>
         ))}
