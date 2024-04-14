b7389825beb8d4d52ceec0ab65e96198b3959b13
Alex Brewster <alexgbrewster@gmail.com>
2024-04-14 10:00:10 -0400

    Update NavMenu.tsx

--- components/NavMenu.tsx
+++ components/NavMenu.tsx
@@ -6,11 +6,11 @@ import Sidebar from "./Sidebar";
 export default function NavMenu() {
   const [sidebar, setSidebar] = useState(false);
   return (
-    <div className="flex flex-row pl-4">
+    <div className="flex flex-row">
       {/* Toggles sidebar on click */}
       {sidebar && <Sidebar />}
       <FaBars
-        className="w-8 h-8 md:hidden cursor-pointer"
+        className="size-8 md:hidden cursor-pointer"
         onClick={() => setSidebar((prev) => !prev)}
       />
     </div>
