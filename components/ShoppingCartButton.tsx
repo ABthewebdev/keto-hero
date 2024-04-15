288df15dc347a8a50b79f264c28b904036384c83
Alex Brewster <alexgbrewster@gmail.com>
2024-04-15 16:52:54 -0400

    Update ShoppingCartButton.tsx

--- components/ShoppingCartButton.tsx
+++ components/ShoppingCartButton.tsx
@@ -1,7 +1,6 @@
 import { ShoppingCart } from "@/lib/db/cart";
 import { FiShoppingCart } from "react-icons/fi";
 import { Badge } from "./ui/badge";
-import { formatPrice } from "@/lib/format";
 
 interface ShoppingCartButtonProps {
   cart: ShoppingCart | null;
@@ -9,21 +8,12 @@ interface ShoppingCartButtonProps {
 
 export default function ShoppingCartButton({ cart }: ShoppingCartButtonProps) {
   return (
-    <div>
-      <div className="flex items-center space-x-3 mt-1">
+    <div className="relative inline-flex flex-col">
+      <div className="flex items-center cursor-pointer space-x-3 mt-1">
         <FiShoppingCart className="size-8 absolute" />
         <Badge className="text-xs relative bottom-3 left-2">
           {cart?.size || 0}
         </Badge>
-      </div>
-      <div
-        tabIndex={0}
-        className="absolute mt-3 w-52 bg-base-100 shadow rounded-lg"
-      >
-        <div>
-          <span className="text-lg font-bold">{cart?.size || 0} Items</span>
-          <span className="">Subtotal: {formatPrice(cart?.subtotal || 0)}</span>
-        </div>
       </div>
     </div>
   );
