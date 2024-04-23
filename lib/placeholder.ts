d405fb88c1824dae6fd2e587474429e845586c58
Alex Brewster <alexgbrewster@gmail.com>
2024-04-22 22:38:47 -0400

    recipes data

---
+++ lib/placeholder.ts
@@ -0,0 +1,12 @@
+const recipesData = [
+  {
+    id: 1,
+    imageUrl: "/omelette.jpg",
+    name: "Jalapeno Omelette",
+  },
+  {
+    id: 2,
+    imageUrl: "/waffles.webp",
+    name: "5 minute waffles",
+  },
+];
